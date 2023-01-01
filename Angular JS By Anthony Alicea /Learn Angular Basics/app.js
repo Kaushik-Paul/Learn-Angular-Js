476f39cbfec548275c78f7baa54ebf72564e8005
kaushikpaul <kaushik.paul755@gmail.com>
2023-01-01 20:19:46 +0530

    Add Section To Learn from Anthony

---
+++ Angular JS By Anthony Alicea /Learn Angular Basics/app.js
@@ -0,0 +1,14 @@
+let myApp = angular.module("myApp", []);
+
+(function () {
+    let mainController = function ($scope, $log) {
+        $scope.firstname = "John Doe";
+        console.log($scope);
+        $log.log("Hello World Log !!");
+        $log.info("Hello World Info !!");
+        $log.debug("Hello World Debug !!");
+        $log.warn("Hello World Warn !!");
+        $log.error("Hello World Error !!");
+    };
+    myApp.controller("mainController", mainController);
+}());
