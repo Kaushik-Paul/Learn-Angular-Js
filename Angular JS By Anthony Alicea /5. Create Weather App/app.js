b4213b59c27f50b90a9622bcc2f87413bcca6c0f
kaushikpaul <kaushik.paul755@gmail.com>
2023-01-04 14:48:28 +0530

    Add Controller and Custom Service to Weather App

--- Angular JS By Anthony Alicea /5. Create Weather App/app.js
+++ Angular JS By Anthony Alicea /5. Create Weather App/app.js
@@ -1,9 +1,52 @@
 let weatherApp = angular.module("weatherApp", ["ngRoute", "ngResource"]);
 
+// Routing
+(function () {
+    let routerConfig = function ($routeProvider) {
+        $routeProvider
+            .when("/", {
+                templateUrl: "pages/home-page.html",
+                controller: "homeController"
+            })
+            .when("/forecast", {
+                templateUrl: "pages/forecast-page.html",
+                controller: "forecastController"
+            })
+    }
+    weatherApp.config(routerConfig);
+}());
+
+// Service
+(function () {
+    let cityService = function () {
+        this.city = "New York, NY"
+    }
+    weatherApp.service("cityService", cityService);
+}());
+
+
+// Controllers
 (function () {
     let mainController = function ($scope) {
         $scope.name = "John Doe";
     }
 
     weatherApp.controller("mainController", ["$scope", mainController]);
+}());
+
+(function () {
+    let homeController = function ($scope, cityService) {
+        $scope.city = cityService.city;
+        $scope.$watch("city", function () {
+            cityService.city = $scope.city;
+        })
+    }
+    weatherApp.controller("homeController", ["$scope", "cityService", homeController]);
+}());
+
+(function () {
+    let forecastController = function ($scope, cityService) {
+        $scope.city = cityService.city;
+    }
+    weatherApp.controller("forecastController", ["$scope", "cityService", forecastController]);
 }());
