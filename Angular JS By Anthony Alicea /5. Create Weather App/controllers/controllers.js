cd1b102a144ff7ae0825991897c5d0cf014d4d34
kaushikpaul <kaushik.paul755@gmail.com>
2023-01-04 18:30:33 +0530

    Minor Change

--- Angular JS By Anthony Alicea /5. Create Weather App/controllers/controllers.js
+++ Angular JS By Anthony Alicea /5. Create Weather App/controllers/controllers.js
@@ -8,36 +8,42 @@
 }());
 
 (function () {
-    let homeController = function ($scope, cityService) {
+    let homeController = function ($scope, $location, cityService) {
         $scope.city = cityService.city;
         $scope.$watch("city", function () {
             cityService.city = $scope.city;
-        })
+        });
+        $scope.submit = function () {
+            $location.path("/forecast");
+        }
     }
-    weatherApp.controller("homeController", ["$scope", "cityService", homeController]);
+    weatherApp.controller("homeController", ["$scope", "$location", "cityService", homeController]);
 }());
 
 (function () {
-    let forecastController = function ($scope, $resource, $routeParams ,cityService) {
+    let forecastController = function ($scope, $routeParams ,cityService, weatherService) {
         $scope.city = cityService.city;
         $scope.days = $routeParams.days || "2";
-        $scope.weatherAPI = $resource("https://api.openweathermap.org/data/2.5/forecast/daily",
-            {callback: "JSON_CALLBACK"}, {get: {method: "JSONP"}});
-        $scope.weatherResult = $scope.weatherAPI.get({
-            q: $scope.city,
-            cnt: $scope.days,
-            appid: "55324c0fde2d3512be166559dc6fce73"
-        });
-        console.log($scope.weatherResult);
 
-        // Convert Kelvin to Fahrenheit
+        // $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/weather",
+        //     {callback: "JSON_CALLBACK"}, {get: {method: "JSONP"}});
+        // $scope.weatherResult = $scope.weatherAPI.get({
+        //     q: $scope.city,
+        //     // cnt: $scope.days,
+        //     appid: parseInt("55324c0fde2d3512be166559dc6fce73")
+        // });
+        // console.log($scope.weatherResult);
+
+        $scope.weatherResult = weatherService.getWeather($scope.city, $scope.days);
+
+        // Convert Kelvin to Celsius
         $scope.convertFahrenheitToCelsius = function (degK) {
-            return Math.round((1.8 * (degK - 273)) + 32);
+            return Math.round(degK - 273.15);
         }
         // Convert Epoch date to standard Date
         $scope.convertDate = function (dt) {
             return new Date(dt * 1000);
         }
     };
-    weatherApp.controller("forecastController", ["$scope", "$resource", "$routeParams", "cityService", forecastController]);
+    weatherApp.controller("forecastController", ["$scope", "$routeParams", "cityService", "weatherService", forecastController]);
 }());
--- Angular JS By Anthony Alicea /5. Create Weather App/services/service.js
+++ Angular JS By Anthony Alicea /5. Create Weather App/services/service.js
@@ -1,7 +1,26 @@
 // Service
 (function () {
     let cityService = function () {
-        this.city = "New York, NY"
+        this.city = "New York"
     }
     weatherApp.service("cityService", cityService);
+}());
+
+(function () {
+    let weatherService = function ($resource) {
+        this.getWeather = function (city, days) {
+            let weatherUrl = "https://api.openweathermap.org/data/2.5/weather";
+            let weatherAPI = $resource(weatherUrl);
+
+            let weatherResult = weatherAPI.get({
+                q: city,
+                // cnt: days,
+                appid: "55324c0fde2d3512be166559dc6fce73"
+            });
+
+            console.log(weatherResult);
+            return weatherResult;
+        }
+    }
+    weatherApp.service("weatherService", ["$resource", weatherService]);
 }());
