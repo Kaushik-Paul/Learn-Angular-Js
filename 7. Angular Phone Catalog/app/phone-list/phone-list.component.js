24d8aa42f2e6188fa2d6bad624af7227627e3331
kaushik-paul <kaushik.paul755@gmail.com>
2023-01-21 20:41:22 +0530

    Fetch Data from rest

--- 7. Angular Phone Catalog/app/phone-list/phone-list.component.js
+++ 7. Angular Phone Catalog/app/phone-list/phone-list.component.js
@@ -5,12 +5,18 @@ angular.
   module('phoneList').
   component('phoneList', {
     templateUrl: 'phone-list/phone-list.template.html',
-    controller: ['$http', function PhoneListController($http) {
-      let self = this;
-      self.orderProp = 'age';
-
-      $http.get('phones/phones.json').then(function (response) {
-        self.phones = response.data;
-      });
-    }]
+    // controller: ['$http', function PhoneListController($http) {
+    //   let self = this;
+    //   self.orderProp = 'age';
+    //
+    //   $http.get('phones/phones.json').then(function (response) {
+    //     self.phones = response.data;
+    //   });
+    // }]
+    controller: ['Phone',
+      function PhoneListController(Phone) {
+        this.phones = Phone.query();
+        this.orderProp = 'age';
+      }
+    ]
   });
--- 7. Angular Phone Catalog/app/phone-list/phone-list.module.js
+++ 7. Angular Phone Catalog/app/phone-list/phone-list.module.js
@@ -1,5 +1,5 @@
 'use strict';
 
 // Define the `phoneList` module
-angular.module('phoneList', []);
+angular.module('phoneList', ["core.phone"]);
 
